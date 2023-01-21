76e87b9127edb8969ee1ba829d51cd8e9ff8ed01
Gauthier Studios <jesse@gauthierstudios.ca>
2023-01-21 18:40:59 -0500

    .

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -3,46 +3,48 @@ function mobileMenu() {
     "use strict";
     let navbar = document.getElementById("menu");
     let icon = document.getElementById("icon");
-    let animation = document.getElementById("remove-animation");
 
     // Close menu
     if (navbar.style.display === "inline-block") {
-        navbar.style.display = "none";
-        icon.style.color = "#fff";
-        // animation.classList.add("load-blink");
+        icon.classList.remove("icon-hover");
+        navbar.style.animation = "slide-out-top 1s ease-in-out backwards";
+        setTimeout(function () {
+            navbar.style.display = "none";
+        }, 900);
+
+
+
     }
+
      // Open Menu
 
     else {
         navbar.style.display = "inline-block";
-        icon.style.color = "#FF5E5B";
-        // animation.classList.remove("load-blink");
+        navbar.style.animation = "";
+        icon.classList.add("icon-hover");
     }
 
 
 }
 
 
-let viewpoint = document.getElementsByClassName('viewpoint')[0];
+let menuNav = document.getElementsByClassName('menu-nav')[0];
 let menuitems = document.getElementsByClassName('menu-items')[0];
 let icon = document.getElementById("icon");
-let delay = 1000;
 
 // trigger this function every time the user scrolls
 window.onscroll = function (event) {
     var scroll = window.pageYOffset;
     if (scroll >= 710) {
         // blue
-        viewpoint.style.backgroundColor = 'var(--text-red)';
-
-
+        menuNav.style.backgroundColor = 'var(--text-red)';
     }
     else {
         // purple
-        viewpoint.style.backgroundColor = '';
+        menuNav.style.backgroundColor = '';
     }
-
-
 }
+
+
 
 
